fec90890fd38f5b5501564d0e384162d89167847
yavuzyall <yyalcin41@gmail.com>
2022-09-15 19:51:58 +0300

    Add project files.

---
+++ CreationDictionary/DictionaryClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreationDictionary
+{
+    class DictionaryClass<K, V>
+    {
+        KeyValuePair<K, V>[] items;
+
+        public DictionaryClass()
+        {
+            items = new KeyValuePair<K, V>[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            KeyValuePair<K, V>[] tempArray = items;
+            items = new KeyValuePair<K, V>[tempArray.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i]; 
+            }
+            items[items.Length - 1] = new KeyValuePair<K, V>(key, value);
+        }
+        public void ShowList()
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Key + " --> " + item.Value);
+            }
+            
+        }
+    }
+}
---
+++ CreationDictionary/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CreationDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DictionaryClass<int, string> numberPlateCity = new DictionaryClass<int, string>();
+            numberPlateCity.Add(1,"Adana");
+            numberPlateCity.ShowList(); //MyDictionary completed....
+        }
+    }
+}
